f625c72f815e7c040f339cae8cd3100b971bd6b5
Yves Greatti <ygrepo@gmail.com>
2025-05-21 15:22:08 -0400

    Added correction of pop for OV _ 2

--- qsubs/submit_generate_incidence_plots.sh
+++ qsubs/submit_generate_incidence_plots.sh
@@ -55,14 +55,63 @@
 #   "OV BRCA2 indelseg_deletion"
 #   )
   
+# params=(
+#   "BRCA BRCA1 cna"
+#   "BRCA BRCA1 cnaseg"
+#   "BRCA BRCA2 cna"
+#   "BRCA BRCA2 cnaseg"
+#   "OV   BRCA1 cna"
+#   "OV   BRCA1 cnaseg"
+#   )
+  
 params=(
+  "BRCA BRCA1 snv"
+  "BRCA BRCA2 snv"
+  "OV BRCA1 snv"
+  "OV BRCA2 snv"
   "BRCA BRCA1 cna"
   "BRCA BRCA1 cnaseg"
   "BRCA BRCA2 cna"
   "BRCA BRCA2 cnaseg"
+  "BRCA BRCA1 amplification"
+  "BRCA BRCA1 amplificationseg"
+  "BRCA BRCA2 amplification"
+  "BRCA BRCA2 amplificationseg"
   "OV   BRCA1 cna"
   "OV   BRCA1 cnaseg"
+  "OV   BRCA1 amplification"
+  "OV   BRCA1 amplificationseg"
+  "OV   BRCA2 cna"
+  "OV   BRCA2 cnaseg"
+  "OV   BRCA2 amplification"
+  "OV   BRCA2 amplificationseg"
+  "BRCA BRCA1 deletion"
+  "BRCA BRCA2 deletion"
+  "OV BRCA1 deletion"
+  "OV BRCA2 deletion"
+  "BRCA BRCA1 deletionseg"
+  "BRCA BRCA2 deletionseg"
+  "OV BRCA1 deletionseg"
+  "OV BRCA2 deletionseg"
+  "BRCA BRCA1 indel_insertion"
+  "BRCA BRCA2 indel_insertion"
+  "OV BRCA1 indel_insertion"
+  "OV BRCA2 indel_insertion"
+  "BRCA BRCA1 indelseg_insertion"
+  "BRCA BRCA2 indelseg_insertion"
+  "OV BRCA1 indelseg_insertion"
+  "OV BRCA2 indelseg_insertion"
+  "BRCA BRCA1 indel_deletion"
+  "BRCA BRCA2 indel_deletion"
+  "OV BRCA1 indel_deletion"
+  "OV BRCA2 indel_deletion"
+  "BRCA BRCA1 indelseg_deletion"
+  "BRCA BRCA2 indelseg_deletion"
+  "OV BRCA1 indelseg_deletion"
+  "OV BRCA2 indelseg_deletion"
   )
+  
+    
   
 # make sure log dir exists
 mkdir -p logs
